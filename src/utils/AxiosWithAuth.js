6a0bea9464b131ab22868f0da930bd8a25b1a886
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-22 07:46:49 -0500

    created axiosWithAuth

---
+++ src/utils/AxiosWithAuth.js
@@ -0,0 +1,12 @@
+/*dependencies*/
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+  return axios.create({
+    baseURL: "",
+    headers: {
+      Authorization: token
+    }
+  });
+};
