b5a78738b0693727e95ca411d1f9801452a63293
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-22 07:26:38 -0500

    set up Index.js with store, middleware, and routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+/*dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+
+/*components */
 import './index.css';
 import App from './App';
+import { reducer } from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*store*/
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+<Provider store={store}>
+    <Router>
+<App />
+</Router>
+</Provider>,
+ document.getElementById('root'));
 
