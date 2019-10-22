cd3ab6fddf63042b5bde3aa40547f94a70a5135c
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-22 08:51:41 -0500

    created basic login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,53 @@
 /*dependencies*/
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/AxiosWithAuth';
+import { Redirect } from 'react-router-dom';
+import { Form, FormGroup, Label, Input, FormFeedback, FormText, Button } from 'react-strap'
+
+const LoginForm = (props) => {
+    const [credentials, setCredentials] = useState({});
+
+    const handleChange = e => {
+        setCredentials({
+        ...credentials,
+        [e.target.name]: e.target.value
+        });
+    };
+
+    const login = e => {
+        e.preventDefault();
+    }
+
+    return(
+        localStorage.getItem('token') ? (
+            <Redirect to='/home' />
+        ) : (
+            <>
+            <Form onSubmit={login}>
+                <FormGroup>
+                    <Label for='username' hidden>Username</Label>
+                    <Input type = 'username'
+                            name = 'username'
+                            placeholder = 'Username'
+                            value={credentials.username}
+                            onChange = {handleChange}
+                            />
+                </FormGroup>
+                <FormGroup>
+                <Label for='password' hidden>Password</Label>
+                    <Input type = 'password'
+                            name = 'password'
+                            placeholder = 'Password'
+                            value = {credentials.username}
+                            onChange = {handleChange}
+                            />
+                </FormGroup>
+                <Button>Log In</Button>
+            </Form>
+            </>
+        )
+    )
+
+}
+
+export default LoginForm
