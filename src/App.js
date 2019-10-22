592485a8debd26343cdc45e54e420e2bd33837e4
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-22 09:15:45 -0500

    login functional; correctly communicating with backend endpoints

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+/*dependencies */
 import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/*components*/
 import './App.css';
+import LoginForm from './components/Login';
+import PrivateRoute from './utils/PrivateRoute';
+import Navbar from './components/Navbar';
+import ReceiptList from './components/ReceiptList';
 
 function App() {
   return (
-    <div></div>
+    <div>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={LoginForm} />
+          <PrivateRoute path='/receipts' component={ReceiptList} />
+          <Route component={LoginForm} />
+        </Switch>
+    </div>
   );
 }
 
