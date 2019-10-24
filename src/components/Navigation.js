ce1c40890c82faab5b43d985a1fdb6989a5e22b1
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-24 14:52:22 -0500

    updated the navigation component to use routes instead of hrefs, which fixed state bug

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { NavLink as RRNavlink } from 'react-router-dom'
 
 const Navigation = () => {
     return (
         <div>
           <Nav>
             <NavItem>
-              <NavLink href="/">Home</NavLink>
+              <NavLink to='/home' tag={RRNavlink} >Home</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink href="/receipts">Receipts</NavLink>
+                <NavLink to="/receipts"  tag={RRNavlink}>Receipts</NavLink>
             </NavItem>  
             <NavItem>
-                <NavLink href="/add-receipt">Add a Receipt</NavLink>
+                <NavLink to="/add-receipt"  tag={RRNavlink}>Add a Receipt</NavLink>
             </NavItem> 
          </Nav>
         </div>
