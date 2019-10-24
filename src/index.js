1a15d35f0246a89037c2ec553e285bd6ec987303
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-24 13:16:43 -0500

    clean branch for debugging

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { compose } from "redux";
 import logger from 'redux-logger';
+
 /*components */
 import "./index.css";
 import App from "./App";
