53a2da6068ae7538ce37789da1d0ae022da702c6
Mike Harley <mharley12345@gmail.com>
2020-02-24 01:26:15 -0800

    changes url to hosted BE

--- src/utils/AxiosWithAuth.js
+++ src/utils/AxiosWithAuth.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://api-receipt-tracker.herokuapp.com/api",
     headers: {
       Authorization: token
     }
